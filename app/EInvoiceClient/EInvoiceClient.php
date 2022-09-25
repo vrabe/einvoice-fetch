1506d10862cbc91940f34254ad6b5c3fb92f26e4
vrabe <git@vrabe.tw>
2022-09-25 23:03:58 +0800

    add getEInvoiceByQRCode in EInvoiceClient

--- app/EInvoiceClient/EInvoiceClient.php
+++ app/EInvoiceClient/EInvoiceClient.php
@@ -34,7 +34,80 @@ class EInvoiceClient
     }
 
     /**
-     * Get the detail about an e-invoice that you know it's random number.
+     * Check the request result is success or not.
+     *
+     * @param object $result
+     * @return void
+     */
+    private function checkResult(object $result): void
+    {
+        if ($result === null) {
+            throw new EInvoiceResponseException("回應不是 JSON");
+        }
+
+        if ($result->code != "200") {
+            throw new EInvoiceResponseException($result->msg, $result->code);
+        }
+
+        if (property_exists($result, "invStatus") && $result->invStatus === "該筆發票並無開立") {
+            throw new EInvoiceResponseException("該筆發票並無開立");
+        }
+    }
+
+    /**
+     * Generate a EInvoice instance from a request result.
+     *
+     * @param object $result
+     * @param string|null $amount External amount source
+     * @return EInvoice
+     */
+    private function resultToEInvoice(object $result, ?string $amount = null): EInvoice
+    {
+        /*
+            trim leading 0s of rowNum because rowNum in the return value of
+            getCarriedEInvoice() doesn't have leading 0s.
+        */
+        foreach ($result->details as $detail) {
+            $detail->rowNum = ltrim($detail->rowNum, "0");
+        }
+
+        $invoice = new EInvoice();
+        $invoice->number = $result->invNum;
+        $invoice->issuedAt = CarbonImmutable::createFromFormat(
+            "YmdH:i:s",
+            $result->invDate . $result->invoiceTime
+        );
+        $invoice->period = $result->invPeriod;
+        $invoice->status = $result->invStatus;
+        $invoice->sellerName = $result->sellerName;
+        $invoice->sellerAddress = $result->sellerAddress;
+        $invoice->sellerBan = $result->sellerBan;
+        $invoice->currency = $result->currency;
+        $invoice->details = $result->details;
+
+        // buyerBan field may not exist.
+        if (property_exists($result, "buyerBan")) {
+            $invoice->buyerBan = $result->buyerBan;
+        }
+
+        // amount field may not exist.
+        if (property_exists($result, "amount")) {
+            $invoice->amount = $result->amount;
+        } else if ($amount != null) {
+            $invoice->amount = $amount;
+        } else {
+            $sum = 0.0;
+            foreach ($result->details as $detail) {
+                $sum += (float) $detail->amount;
+            }
+            $invoice->amount = (string) $sum;
+        }
+
+        return $invoice;
+    }
+
+    /**
+     * Get the detail about an e-invoice that you know it's random number from data in barcode.
      *
      * @param string $invNum
      * @param string $invDate
@@ -67,51 +140,57 @@ class EInvoiceClient
 
         $result = json_decode((string) $res->getBody());
 
-        if ($result === null) {
-            throw new EInvoiceResponseException("回應不是 JSON");
-        }
+        $this->checkResult($result);
 
-        if ($result->code != "200") {
-            throw new EInvoiceResponseException($result->msg, $result->code);
-        }
+        $invoice = $this->resultToEInvoice($result, $amount);
 
-        if ($result->invStatus === "該筆發票並無開立") {
-            throw new EInvoiceResponseException("該筆發票並無開立");
-        }
-
-        $invoice = new EInvoice();
-        $invoice->number = $result->invNum;
-        $invoice->issuedAt = CarbonImmutable::createFromFormat(
-            "YmdH:i:s",
-            $result->invDate . $result->invoiceTime
+        $invoice->note = array(
+            "randomNumber" => $randomNumber
         );
-        $invoice->period = $result->invPeriod;
-        $invoice->status = $result->invStatus;
-        $invoice->sellerName = $result->sellerName;
-        $invoice->sellerAddress = $result->sellerAddress;
-        $invoice->sellerBan = $result->sellerBan;
-        $invoice->buyerBan = $result->buyerBan;
-        $invoice->currency = $result->currency;
-        $invoice->details = $result->details;
 
-        // Get the amount of this einvoice
-        if ($amount == null) {
-            $sum = 0.0;
-            foreach ($result->details as $detail) {
-                $sum += (double) $detail->amount;
-            }
-            $invoice->amount = (string) $sum;
-        } else {
-            $invoice->amount = $amount;
-        }
+        return $invoice;
+    }
 
-        /*
-            trim leading 0s of rowNum because rowNum in the return value of
-            getCarriedEInvoice() doesn't have leading 0s.
-        */
-        foreach ($result->details as $detail) {
-            $detail->rowNum = ltrim($detail->rowNum, "0");
-        }
+    /**
+     * Get the detail about an e-invoice that you know it's random number from data in QRcode.
+     *
+     * @param string $invNum
+     * @param string $invDate
+     * @param string $sellerID
+     * @param string $encrypt
+     * @param string $randomNumber
+     * @param string|null $amount
+     * @return EInvoice
+     */
+    public function getEInvoiceByQRCode(
+        string $invNum,
+        string $invDate,
+        string $sellerID,
+        string $encrypt,
+        string $randomNumber,
+        ?string $amount = null
+    ): EInvoice {
+        $res = $this->client->request("POST", self::URL . "/PB2CAPIVAN/invapp/InvApp", [
+            "form_params" => [
+                "version" => "0.5",
+                "type" => "QRCode",
+                "invNum" => $invNum,
+                "action" => "qryInvDetail",
+                "generation" => "V2",
+                "encrypt" => $encrypt,
+                "sellerID" => $sellerID,
+                "invDate" => $invDate,
+                "UUID" => $this->uuid,
+                "randomNumber" => $randomNumber,
+                "appID" => $this->appID
+            ]
+        ]);
+
+        $result = json_decode((string) $res->getBody());
+
+        $this->checkResult($result);
+
+        $invoice = $this->resultToEInvoice($result, $amount);
 
         $invoice->note = array(
             "randomNumber" => $randomNumber
@@ -153,45 +232,9 @@ class EInvoiceClient
 
         $result = json_decode((string) $res->getBody());
 
-        if ($result === null) {
-            throw new EInvoiceResponseException("回應不是 JSON");
-        }
+        $this->checkResult($result);
 
-        if ($result->code != "200") {
-            throw new EInvoiceResponseException($result->msg, $result->code);
-        }
-
-        if ($result->invStatus === "該筆發票並無開立") {
-            throw new EInvoiceResponseException("該筆發票並無開立");
-        }
-
-        $invoice = new EInvoice();
-        $invoice->number = $result->invNum;
-        $invoice->issuedAt = CarbonImmutable::createFromFormat(
-            "YmdH:i:s",
-            $result->invDate . $result->invoiceTime
-        );
-        $invoice->period = $result->invPeriod;
-        $invoice->status = $result->invStatus;
-        $invoice->sellerName = $result->sellerName;
-        $invoice->sellerAddress = $result->sellerAddress;
-        $invoice->sellerBan = $result->sellerBan;
-        $invoice->currency = $result->currency;
-        $invoice->amount = $result->amount;
-        $invoice->details = $result->details;
-
-        // buyerBan field may not exist.
-        if (property_exists($result, "buyerBan")) {
-            $invoice->buyerBan = $result->buyerBan;
-        }
-
-        /*
-            Even though rowNum in the return value of this method doesn't have leading 0s.
-            I still do it to make sure there are no leading 0s.
-        */
-        foreach ($result->details as $detail) {
-            $detail->rowNum = ltrim($detail->rowNum, "0");
-        }
+        $invoice = $this->resultToEInvoice($result);
 
         return $invoice;
     }
@@ -232,13 +275,7 @@ class EInvoiceClient
 
         $result = json_decode((string) $res->getBody());
 
-        if ($result === null) {
-            throw new EInvoiceResponseException("回應不是 JSON");
-        }
-
-        if ($result->code != "200") {
-            throw new EInvoiceResponseException($result->msg, $result->code);
-        }
+        $this->checkResult($result);
 
         $invoiceList = array();
 
@@ -252,7 +289,7 @@ class EInvoiceClient
                 $detail->invDate->time,
                 "Asia/Taipei"
             )->format("Y/m/d");
-            
+
             $invoiceList[] = $invoiceData;
         }
 
--- tests/Unit/EInvoiceClient/EInvoiceClientTest.php
+++ tests/Unit/EInvoiceClient/EInvoiceClientTest.php
@@ -86,6 +86,82 @@ class EInvoiceClientTest extends TestCase
         );
     }
 
+    public function testGetEInvoiceByQRCode(): void
+    {
+        $handle = fopen(__dir__ . "/EInvoiceTestInput01.json", "r");
+        $sucessResult = fread($handle, filesize(__dir__ . "/EInvoiceTestInput01.json"));
+        fclose($handle);
+
+        $failResult = '{"v":"0.5","code":904,"msg":"錯誤的查詢種類"}';
+
+        $mock = new MockHandler([
+            new Response(200, [], $sucessResult),
+            new Response(200, [], $sucessResult),
+            new Response(200, [], $failResult),
+            new Response(200, [], "<html></html>"),
+            new Response(503)
+        ]);
+        $mockHttpClient = new Client(["handler" => HandlerStack::create($mock)]);
+        $eInvoiceClient = new EInvoiceClient($mockHttpClient);
+
+        // Normal case
+        $invoice = $eInvoiceClient->getEInvoiceByQRCode(
+            "MH25570631",
+            "2019/01/19",
+            "72452657",
+            "g++HDeJS3GgXiHu8ACvkIg==",
+            "2355"
+        );
+        $this->assertJsonStringEqualsJsonFile(
+            __DIR__ . "/EInvoiceTestOutput01.json",
+            json_encode($invoice)
+        );
+
+        // Normal case with additional amount
+        $invoice = $eInvoiceClient->getEInvoiceByQRCode(
+            "MH25570631",
+            "2019/01/19",
+            "72452657",
+            "g++HDeJS3GgXiHu8ACvkIg==",
+            "2355",
+            "50"
+        );
+        $this->assertJsonStringEqualsJsonFile(
+            __DIR__ . "/EInvoiceTestOutput01.json",
+            json_encode($invoice)
+        );
+
+        // E-Invoice system error 1
+        $this->expectException(EInvoiceResponseException::class);
+        $invoice = $eInvoiceClient->getEInvoiceByQRCode(
+            "MH25570631",
+            "2019/01/19",
+            "72452657",
+            "g++HDeJS3GgXiHu8ACvkIg==",
+            "2355"
+        );
+
+        // E-Invoice system error 2
+        $this->expectException(EInvoiceResponseException::class);
+        $invoice = $eInvoiceClient->getEInvoiceByQRCode(
+            "MH25570631",
+            "2019/01/19",
+            "72452657",
+            "g++HDeJS3GgXiHu8ACvkIg==",
+            "2355"
+        );
+
+        // Server error
+        $this->expectException(RequestException::class);
+        $invoice = $eInvoiceClient->getEInvoiceByQRCode(
+            "MH25570631",
+            "2019/01/19",
+            "72452657",
+            "g++HDeJS3GgXiHu8ACvkIg==",
+            "2355"
+        );
+    }
+
     public function testCarriedEInvoice(): void
     {
         $handle = fopen(__dir__ . "/EInvoiceTestInput02.json", "r");
@@ -173,13 +249,13 @@ class EInvoiceClientTest extends TestCase
                 "invNum" => "ER02338051",
                 "invDate" => "2012/07/09",
                 "cardType" => "3J0002",
-                "cardNo"=> "/XCCYDHQ"
+                "cardNo" => "/XCCYDHQ"
             ),
             array(
                 "invNum" => "ER02338052",
                 "invDate" => "2012/07/09",
                 "cardType" => "3J0002",
-                "cardNo"=> "/XCCYDHQ"
+                "cardNo" => "/XCCYDHQ"
             )
         );
 
